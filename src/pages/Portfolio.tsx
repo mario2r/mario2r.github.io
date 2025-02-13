129eaa3ee6531ee36b85839103033c0b897df07d
Mario Romero Rodriguez <mario@ifeelonline.com>
2025-02-13 16:03:53 +0100

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,4 +1,3 @@
-//import React from "react";
 import { motion } from "framer-motion";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import ProfileAvatar from "../assets/profile.jpg";
